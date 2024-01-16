2a74a2ce9634a7c141941d81b81475cb5e81f6f2
Leila Zrim <leilazrim@gmail.com>
2024-01-16 11:13:42 +0100

    [js - 06] [Step 03] Get "#head-coach" and "#player-cards"

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const teamName = document.getElementById("team");
 const typeOfSport = document.getElementById("sport");
 const worldCupYear = document.getElementById("year");
+const headCoach = document.getElementById("head-coach");
+const playerCards = document.getElementById("player-cards");
