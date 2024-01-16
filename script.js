4b8d198c6d90b641f72b747a7ebc5c433cffe459
Leila Zrim <leilazrim@gmail.com>
2024-01-16 11:47:50 +0100

    [js - 06] [Step 26] Set the "playerCards" to the new array

--- script.js
+++ script.js
@@ -183,4 +183,6 @@ teamName.textContent = team;
 worldCupYear.textContent = year;
 headCoach.textContent = coachName;
 
-const setPlayerCards = (arr = players) => {};
+const setPlayerCards = (arr = players) => {
+  playerCards.innerHTML += arr.map(() => {});
+};
