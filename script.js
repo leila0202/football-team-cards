c1b83ad40cd27ca7b705983559027e9c9860bb74
Leila Zrim <leilazrim@gmail.com>
2024-01-16 12:01:19 +0100

    [js - 06] [Step 34] Add nickname display

--- script.js
+++ script.js
@@ -192,7 +192,7 @@ const setPlayerCards = (arr = players) => {
         <h2>${name} ${isCaptain ? "(Captain)" : ""}</h2>
         <p>Position: ${position}</p>
         <p>Number: ${number}</p>
-        <p>Nickname: </p>
+        <p>Nickname: ${nickname !== null ? nickname : "N/A"}</p>
       </div>`;
     }
   );
