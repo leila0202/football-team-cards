71055f1639c0f5311710aa7cb1361e2dde6119d1
Leila Zrim <leilazrim@gmail.com>
2024-01-16 11:21:49 +0100

    [js - 06] [Step 10] Add property "headCoach"

--- script.js
+++ script.js
@@ -4,10 +4,17 @@ const worldCupYear = document.getElementById("year");
 const headCoach = document.getElementById("head-coach");
 const playerCards = document.getElementById("player-cards");
 const playersDropdownList = document.getElementById("players");
+/*Step 10
 
+Below the isWorldCupWinner property, add a new key called headCoach with a value of an empty object. Inside that object, add a property with a key of coachName and a string value of Carlos Bilardo. Below that property, add another key called matches with a number value of 7.
+*/
 const myFavoriteFootballTeam = {
   team: "Argentina",
   sport: "Football",
   year: 1986,
   isWorldCupWinner: true,
+  headCoach: {
+    coachName: "Carlos Bilardo",
+    matches: 7,
+  },
 };
