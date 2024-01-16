32072b3cd5141f2dd9ff3469e8ea17b361d86132
Leila Zrim <leilazrim@gmail.com>
2024-01-16 11:14:59 +0100

    [js - 06] [Step 04] Get "#players"

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const typeOfSport = document.getElementById("sport");
 const worldCupYear = document.getElementById("year");
 const headCoach = document.getElementById("head-coach");
 const playerCards = document.getElementById("player-cards");
+/*Step 4
+
+Create one more const variable called playersDropdownList and assign it the id of players using the getElementById method.
+*/
+const playersDropdownList = document.getElementById("players");
