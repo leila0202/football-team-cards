4838999564c44f621a18aee36f91b9dda173817a
Leila Zrim <leilazrim@gmail.com>
2024-01-16 11:41:04 +0100

    [js - 06] [Step 23] Render "year" and "coachName" properties

--- script.js
+++ script.js
@@ -180,3 +180,5 @@ const { coachName } = myFavoriteFootballTeam.headCoach;
 
 typeOfSport.textContent = sport;
 teamName.textContent = team;
+worldCupYear.textContent = year;
+headCoach.textContent = coachName;
