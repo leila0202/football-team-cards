5fab6354940c24431b1d5b5961428310788cbee4
Leila Zrim <leilazrim@gmail.com>
2024-01-16 12:44:15 +0100

    [js - 06] [Step 41] Check the first case

--- script.js
+++ script.js
@@ -201,5 +201,6 @@ const setPlayerCards = (arr = players) => {
 playersDropdownList.addEventListener("change", (e) => {
   playerCards.innerHTML = "";
   switch (e.target.value) {
+    case "nickname":
   }
 });
