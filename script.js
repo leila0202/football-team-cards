cb0234a5b8f3fd97127419e7f769a42cb6968e31
Leila Zrim <leilazrim@gmail.com>
2024-01-16 11:53:49 +0100

    [js - 06] [Step 29] Add "name" parameter to the div

--- script.js
+++ script.js
@@ -186,7 +186,9 @@ headCoach.textContent = coachName;
 const setPlayerCards = (arr = players) => {
   playerCards.innerHTML += arr.map(
     ({ name, position, number, isCaptain, nickname }) => {
-      `<div class="player-card"></div>`;
+      `<div class="player-card">
+        <h2>${name}</h2>
+      </div>`;
     }
   );
 };
