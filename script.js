75681e92e3d3031222ee8133ef6b3b34b97323b8
Leila Zrim <leilazrim@gmail.com>
2024-01-16 12:40:25 +0100

    [js - 06] [Step 38] Add console log to show how "e" works

--- script.js
+++ script.js
@@ -198,4 +198,6 @@ const setPlayerCards = (arr = players) => {
     .join("");
 };
 
-playersDropdownList.addEventListener("change", (e) => {});
+playersDropdownList.addEventListener("change", (e) => {
+  console.log(e.target.value);
+});
